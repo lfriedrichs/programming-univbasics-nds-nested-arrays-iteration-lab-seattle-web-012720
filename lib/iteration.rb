b1672a3d78a33016445f905a24a70225f14bddf9
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-15 02:01:40 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -36,7 +36,16 @@ def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
-  #
+  index = 0 
+  total = 0
+  while src[index]
+    if src[index][0]%2 == 0 && src[index][1] %2 == 0
+      total += src[index][0]+src[index][1]
+    end
+    index += 1
+  end
+  total
+end
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
