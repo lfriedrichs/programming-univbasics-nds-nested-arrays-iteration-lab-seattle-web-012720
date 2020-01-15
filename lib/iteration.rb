8ed0f17070f8935c8de83b959e376eb5701678d4
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-01-15 01:53:46 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -24,8 +24,8 @@ def find_greater_pair(src)
   while src[index]
     if src[index][0] > src[index][1] {
       new_Array[index] = [src[index][0], src[index][1]]
-    } else {
-      new_Array[index] = [src[index][1], src[index][0]]
+    } #else {
+      #new_Array[index] = [src[index][1], src[index][0]]
     }
     index += 1
   end
